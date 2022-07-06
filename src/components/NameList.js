d45fae8419ee17d0026d3205b8225787c1e5b95a
Rohit <rohitdpatil23@gmail.com>
2022-07-06 19:01:13 +0530

    2.List Rendering using map() method

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,13 +2,8 @@ import React from "react";
 
 function NameList() {
   const names = ["Bruce", "Clark", "Diana"];
-  return (
-    <div>
-      {names.map((name) => (
-        <h2>{name}</h2>
-      ))}
-    </div>
-  );
+  const NameList = names.map((name) => <h2>{name}</h2>);
+  return <div>{NameList}</div>;
 }
 
 export default NameList;
