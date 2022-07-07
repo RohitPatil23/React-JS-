5460003cb9beebc31cb204d3ee12770764e9982f
Rohit <rohitdpatil23@gmail.com>
2022-07-07 15:41:19 +0530

    CSS Stylesheet

--- src/App.js
+++ src/App.js
@@ -12,11 +12,13 @@ import ParentComponent from "./components/ParentComponent";
 import UserGreeting from "./components/UserGreeting";
 import NameList from "./components/NameList";
 import PersonList from "./components/NameList";
+import Stylesheet from "./components/Stylesheet";
 
 function App() {
   return (
     <div className="App">
-    <PersonList />
+      <Stylesheet primary={false}/>
+      {/* <PersonList /> */}
       {/* <NameList /> */}
       {/* <UserGreeting /> */}
       {/* <ParentComponent /> */}
