119e5575adebc26253e1e5396fba0de5b745e44e
Rohit <rohitdpatil23@gmail.com>
2022-07-07 15:02:32 +0530

    Index as Key

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,26 +1,26 @@
 import React from "react";
 import Person from "./Person";
 
-// function NameList() {
-//   const names = ["Bruce", "Clark", "Diana"];
-//   const NameList = names.map((name) => <h2>{name}</h2>);
-//   return <div>{NameList}</div>;
-// }
-
-function PersonList() {
-  const persons = [
-    { id: 1, name: "Bruce", age: 30, skill: "React" },
-    { id: 2, name: "Clark", age: 25, skill: "Angular" },
-    { id: 3, name: "Diana", age: 28, skill: "Vue" },
-  ];
-  // person as a parameter which is passed as the prop to the person component
-  //Each child in a list should have a unique "key" prop
-  const PersonList = persons.map((person) => (
-    <Person key={person.id} person={person}></Person>
-  ));
-  return <div>{PersonList}</div>;
+function NameList() {
+  const names = ["Bruce", "Clark", "Diana","Bruce"];
+  const NameList = names.map((name,index) => <h2 key={index}>{index}{name}</h2>);
+  return <div>{NameList}</div>;
 }
 
-export default PersonList;
+// function PersonList() {
+//   const persons = [
+//     { id: 1, name: "Bruce", age: 30, skill: "React" },
+//     { id: 2, name: "Clark", age: 25, skill: "Angular" },
+//     { id: 3, name: "Diana", age: 28, skill: "Vue" },
+//   ];
+//   // person as a parameter which is passed as the prop to the person component
+//   //Each child in a list should have a unique "key" prop
+//   const PersonList = persons.map((person) => (
+//     <Person key={person.id} person={person}></Person>
+//   ));
+//   return <div>{PersonList}</div>;
+// }
 
-//export default NameList;
+// export default PersonList;
+
+export default NameList;
