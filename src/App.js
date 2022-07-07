2e76345623dcdc97d1e088e7945c5893f6908ad9
Rohit <rohitdpatil23@gmail.com>
2022-07-07 15:50:08 +0530

    Inline CSS

--- src/App.js
+++ src/App.js
@@ -13,11 +13,14 @@ import UserGreeting from "./components/UserGreeting";
 import NameList from "./components/NameList";
 import PersonList from "./components/NameList";
 import Stylesheet from "./components/Stylesheet";
+import Inline from "./components/Inline";
 
 function App() {
   return (
     <div className="App">
-      <Stylesheet primary={false}/>
+     
+      <Inline />
+      {/* <Stylesheet primary={false}/> */}
       {/* <PersonList /> */}
       {/* <NameList /> */}
       {/* <UserGreeting /> */}
