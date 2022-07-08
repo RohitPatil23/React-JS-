51998e66167c8f7ebf501d0e1c6ccf14e15cb610
Rohit <rohitdpatil23@gmail.com>
2022-07-08 18:44:24 +0530

    Component Updating Lifecycle Methods

--- src/components/LifecycleA.js
+++ src/components/LifecycleA.js
@@ -18,12 +18,31 @@ class LifecycleA extends Component {
   componentDidMount() {
     console.log("LifecycleA componentDidMount");
   }
+  changeState=()=>{
+    this.setState({
+        name : 'changeState'
+    })
+  }
+  shouldComponentUpdate() {
+    console.log("LifecycleA shouldComponentUpdate");
+    return true;
+  }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+      console.log("LifecycleA getSnapshotBeforeUpdate");
+   return null;
+  }
+  componentDidUpdate() {
+    console.log("LifecycleA componentDidUpdate");
+  }
   render() {
     console.log("LifecycleA render");
-    return (<div>
+    return (
+      <div>
         <div>Lifecycle A</div>
+        <button onClick={this.changeState}>Change State</button>
         <LifecycleB />
-        </div>) 
+      </div>
+    );
   }
 }
 
--- src/components/LifecycleB.js
+++ src/components/LifecycleB.js
@@ -17,6 +17,17 @@ class LifecycleB extends Component {
   componentDidMount() {
     console.log("LifecycleB componentDidMount");
   }
+  shouldComponentUpdate() {
+    console.log("LifecycleB shouldComponentUpdate");
+    return true;
+  }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+      console.log("LifecycleB getSnapshotBeforeUpdate");
+      return null;
+  }
+  componentDidUpdate() {
+    console.log("LifecycleB componentDidUpdate");
+  }
   render() {
     console.log("LifecycleB render");
     return <div>Lifecycle B</div>;
